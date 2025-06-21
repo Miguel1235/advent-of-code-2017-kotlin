cf5bf05e32251a270cab9e27014914b05271c1dc
Miguel Del Corso <migueldelcorso@gmail.com>
2025-06-21 10:37:17 -0300

    Improved code with a more functional approach.

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,27 +1,20 @@
-private fun part1(input: List<List<Int>>): Int {
-    return input.fold(0) { acc, row -> acc + (row.max() - row.min()) }
+private val findEvenlyDivisible = { row: List<Int> ->
+    row.indices.asSequence()
+        .flatMap { i -> (i + 1 until row.size).map { j -> row[i] to row[j] } }
+        .firstNotNullOfOrNull { (first, second) ->
+            when {
+                first % second == 0 -> first / second
+                second % first == 0 -> second / first
+                else -> null
+            }
+        } ?: 0
 }
 
-private fun part2(input: List<List<Int>>): Int {
-    return  input.fold(0) { acc, row -> acc + findEvenlyDivisible(row) }
-}
+private val parseInput = { input: List<String> -> input.map { it.split("""\s+""".toRegex()).map { it.toInt() } } }
 
-private fun findEvenlyDivisible(row: List<Int>): Int {
-    for(i in 0 until row.size) {
-        for(j in i+1 until row.size) {
-            val f = row[i]
-            val s = row[j]
+private val part1 = { input: List<List<Int>> -> input.sumOf { it.max() - it.min() } }
+private val part2 = { input: List<List<Int>> -> input.sumOf { findEvenlyDivisible(it) } }
 
-            if(f%s == 0) return f/s
-            if(s%f == 0) return s/f
-        }
-    }
-    return 0
-}
-
-private fun parseInput(input: List<String>): List<List<Int>> {
-    return input.map { it.split("""\s+""".toRegex()).map { it.toInt() } }
-}
 
 fun main() {
     val testInput = parseInput(readInput("Day02_test"))
