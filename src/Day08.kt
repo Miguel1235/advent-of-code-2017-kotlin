a587b52294eee59dc8d49ae2d903fd49797e96e4
Miguel Del Corso <migueldelcorso@gmail.com>
2025-07-02 11:00:37 -0300

    Made some refactors

--- src/Day08.kt
+++ src/Day08.kt
@@ -9,36 +9,55 @@ fun updateRegisterValue(operation: String, registers: MutableMap<String, Int>, r
 private fun part1(instructions: List<Instruction>, isPart1: Boolean = true): Int {
     val registers = startRegisters(instructions)
     var maxRegistered = 0
-    for(instruction in instructions) {
+    for (instruction in instructions) {
         val (register, operation, amount, operand1, comparatorOperator, operand2) = instruction
         val operand1Value = registers[operand1]!!
-        when(comparatorOperator) {
-            ComparatorOperator.EQUAL -> {
-                if(operand1Value == operand2) updateRegisterValue(operation, registers, register, amount)
-            }
-            ComparatorOperator.NOT_EQUAL -> {
-                if(operand1Value != operand2) updateRegisterValue(operation, registers, register, amount)
+        when (comparatorOperator) {
+            ComparatorOperator.EQUAL -> if (operand1Value == operand2) updateRegisterValue(
+                operation,
+                registers,
+                register,
+                amount
+            )
 
-            }
-            ComparatorOperator.LESS_THAN -> {
-                if(operand1Value < operand2) updateRegisterValue(operation, registers, register, amount)
-            }
-            ComparatorOperator.LESS_THAN_OR_EQUAL -> {
-                if(operand1Value <= operand2) updateRegisterValue(operation, registers, register, amount)
+            ComparatorOperator.NOT_EQUAL -> if (operand1Value != operand2) updateRegisterValue(
+                operation,
+                registers,
+                register,
+                amount
+            )
 
-            }
-            ComparatorOperator.GREATER_THAN -> {
-                if(operand1Value > operand2) updateRegisterValue(operation, registers, register, amount)
+            ComparatorOperator.LESS_THAN -> if (operand1Value < operand2) updateRegisterValue(
+                operation,
+                registers,
+                register,
+                amount
+            )
 
-            }
-            ComparatorOperator.GREATER_THAN_OR_EQUAL -> {
-                if(operand1Value >= operand2) updateRegisterValue(operation, registers, register, amount)
+            ComparatorOperator.LESS_THAN_OR_EQUAL -> if (operand1Value <= operand2) updateRegisterValue(
+                operation,
+                registers,
+                register,
+                amount
+            )
 
-            }
+            ComparatorOperator.GREATER_THAN -> if (operand1Value > operand2) updateRegisterValue(
+                operation,
+                registers,
+                register,
+                amount
+            )
+
+            ComparatorOperator.GREATER_THAN_OR_EQUAL -> if (operand1Value >= operand2) updateRegisterValue(
+                operation,
+                registers,
+                register,
+                amount
+            )
         }
 
         val currentMax = registers.maxOf { it.value }
-        if(currentMax > maxRegistered) maxRegistered = currentMax
+        if (currentMax > maxRegistered) maxRegistered = currentMax
     }
     return if (isPart1) registers.maxOf { it.value } else maxRegistered
 }
@@ -49,28 +68,38 @@ enum class ComparatorOperator {
 
 private fun startRegisters(instructions: List<Instruction>): MutableMap<String, Int> {
     return buildMap {
-        for( (register) in instructions) {
+        for ((register) in instructions) {
             putIfAbsent(register, 0)
         }
     }.toMutableMap()
 }
 
-data class Instruction(val register: String, val operation: String, val amount: Int, val operand1: String, val comparatorOperator: ComparatorOperator, val operand2: Int)
+data class Instruction(
+    val register: String,
+    val operation: String,
+    val amount: Int,
+    val operand1: String,
+    val comparatorOperator: ComparatorOperator,
+    val operand2: Int
+)
 
 private fun parseInput(input: List<String>): List<Instruction> {
     val instructionRegex = Regex("""(\w+) (inc|dec) (-?\d+) if (\w+) (==|>|>=|<|<=|!=) (-?\w+)""")
-
     return buildList {
-        for(instruction in input) {
+        for (instruction in input) {
             val regexResult = instructionRegex.matchEntire(instruction)!!.groupValues.drop(1)
-            add(Instruction(regexResult[0], regexResult[1], regexResult[2].toInt(), regexResult[3],when(regexResult[4]) {
+            add(
+                Instruction(
+                    regexResult[0], regexResult[1], regexResult[2].toInt(), regexResult[3], when (regexResult[4]) {
                         "==" -> ComparatorOperator.EQUAL
                         ">" -> ComparatorOperator.GREATER_THAN
                         ">=" -> ComparatorOperator.GREATER_THAN_OR_EQUAL
                         "<" -> ComparatorOperator.LESS_THAN
                         "<=" -> ComparatorOperator.LESS_THAN_OR_EQUAL
                         else -> ComparatorOperator.NOT_EQUAL
-                }, regexResult[5].toInt()))
+                    }, regexResult[5].toInt()
+                )
+            )
         }
     }
 }
@@ -79,7 +108,7 @@ fun main() {
     val testInput = parseInput(readInput("Day08_test"))
     check(part1(testInput) == 1)
     check(part1(testInput, false) == 10)
-     
+
     val input = parseInput(readInput("Day08"))
     check(part1(input) == 5221)
     check(part1(input, false) == 7491)
