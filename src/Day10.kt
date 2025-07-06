7e69bd33249c0013d22a8e59772f4e6b8ac60e11
Miguel Del Corso <migueldelcorso@gmail.com>
2025-07-06 16:38:03 -0300

    Started Day10, not finished yet

---
+++ src/Day10.kt
@@ -0,0 +1,64 @@
+private fun part1(input: List<Int>): Int {
+     var circularList = listOf(0,1,2,3,4)
+
+    var skipSize = 0
+    var currentPosition = circularList.first()
+
+
+    for(length in input) {
+        println("going to reverse $length elements in the list $circularList starting at: $currentPosition")
+
+        println(circularList.subList(currentPosition, length).reversed())
+
+
+        val newCircularList = buildList {
+            addAll(circularList.subList(currentPosition, length).reversed()) // wrap around it
+            addAll(circularList.takeLast((length % circularList.size)-1))
+        }
+
+        circularList = newCircularList
+        println(newCircularList)
+
+
+//        Move the current position forward by that length plus the skip size.
+//        Increase the skip size by one.
+
+        currentPosition = length + skipSize
+        skipSize++
+
+        println("skips size: $skipSize - currentPosition: $currentPosition")
+
+    }
+
+//    for(firstLen in circularList) {
+//        println("skipSize: $skipSize - currentPosition: $currentPosition - firstLen: $firstLen")
+//
+//        var offset = currentPosition + firstLen
+//        val result = circularList.subList(currentPosition, offset).reversed()
+//
+//
+//        val newList = result + circularList.takeLast((offset % circularList.size)-1)
+//
+//        currentPosition = offset + skipSize
+//        skipSize++
+//    }
+
+    return 0
+}
+
+//private fun part2(input: List<String>): Int {
+//    return 0
+//}
+
+private fun parseInput(input: List<String>) = input.first().split(",").map { it.trim().toInt() }
+
+fun main() {
+    val testInput = parseInput(readInput("Day10_test"))
+    check(part1(testInput) == 0)
+//    check(part2(testInput) == 0)
+     
+//    val input = readInput("Day10")
+//    check(part1(input) == 0)
+//    check(part2(input) == 0)
+}
+ 
