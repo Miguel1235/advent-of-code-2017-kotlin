0237863beac690b8c934046cfc93cd3822a17120
Miguel Del Corso <migueldelcorso@gmail.com>
2025-07-08 09:50:56 -0300

    Made yet some more changed, not finished yet

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,21 +1,48 @@
 private fun part1(input: List<Int>): Int {
-     var circularList = listOf(0,1,2,3,4)
+    var circularList = listOf(0,1,2,3,4)
     var skipSize = 0
     var currentPosition = circularList.first()
     for(length in input) {
         println("going to reverse $length elements in the list $circularList starting at: $currentPosition")
-        println(circularList.subList(currentPosition, length).reversed())
+        println("size of the list: ${circularList.size} - length: $length - currentPosition: $currentPosition - skipSize: $skipSize")
+
+
         val newCircularList = buildList {
-            addAll(circularList.subList(currentPosition, length).reversed()) // wrap around it
-            addAll(circularList.takeLast((length % circularList.size)-1)) // this should be changed specif to the wraps
+//            println("$currentPosition - $length - ${circularList.size}")
+            when {
+                length == 1 -> addAll(circularList)
+
+                currentPosition + length > circularList.size -> {
+                    println("[WRAP] we are going to wrap  more than the list size")
+                    val end = circularList.subList(currentPosition, circularList.size).reversed()
+                    val start = circularList.subList(0, length - end.size).reversed()
+                    val middle = circularList.subList(start.size, currentPosition)
+
+
+                    println("start: $start - middle: $middle - end: $end")
+
+
+                    addAll(end)
+                    addAll(middle)
+                    addAll(start)
+                }
+
+                else -> {
+                    addAll(circularList.subList(currentPosition, length).reversed())
+                    addAll(circularList.takeLast((length % circularList.size)-1))
+                }
+
+            }
         }
         circularList = newCircularList
         println(newCircularList)
 //        Move the current position forward by that length plus the skip size.
 //        Increase the skip size by one.
-        currentPosition = length + skipSize
+//        currentPosition = length + skipSize
+        currentPosition = (currentPosition + length + skipSize) % circularList.size
         skipSize++
         println("skips size: $skipSize - currentPosition: $currentPosition")
+        println("*********")
     }
     return 0
 }
