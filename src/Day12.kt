38901dbf35424cb4db2bccd879811d4a2cf0528a
Miguel Del Corso <migueldelcorso@gmail.com>
2025-07-13 19:28:28 -0300

    Made some refactors

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,16 +1,22 @@
-import kotlin.reflect.typeOf
-
-class ProgramGraph {
+class ProgramGraph(input: List<String>) {
     val adjacencyList = mutableMapOf<Int, MutableList<Int>>()
-    fun addNode(node: Int) {
-        adjacencyList.putIfAbsent(node, mutableListOf())
-    }
+    init {
+        for(program in input) {
+            val (from, to) = program.split("<->").map { it.trim() }
 
+            if(to.contains(",")) {
+                to.split(",").map { it.trim() }.forEach {
+                    addEdge(from.toInt(), it.toInt())
+                }
+            } else {
+                addEdge(from.toInt(), to.toInt())
+            }
+        }
+    }
     fun addEdge(from: Int, to: Int) {
-        addNode(from)
+        adjacencyList.putIfAbsent(from, mutableListOf())
         adjacencyList[from]?.add(to)
     }
-
     fun dfs(current: Int, target: Int, visited: MutableSet<Int> = mutableSetOf()): Boolean {
         if(current == target) return true
         visited.add(current)
@@ -18,28 +24,13 @@ class ProgramGraph {
     }
 }
 
-private fun part1(input: List<String>): Int {
-    val programGraph = ProgramGraph()
-
-    for(program in input) {
-        val (from, to) = program.split("<->").map { it.trim() }
-
-        if(to.contains(",")) {
-            to.split(",").map { it.trim() }.forEach {
-                programGraph.addEdge(from.toInt(), it.toInt())
-            }
-        } else {
-            programGraph.addEdge(from.toInt(), to.toInt())
-        }
-    }
-    return programGraph.adjacencyList.keys.count { programGraph.dfs(0, it) }
-}
+private val part1 = { graph: ProgramGraph -> graph.adjacencyList.keys.count { graph.dfs(0, it) } }
 
 fun main() {
-    val testInput = readInput("Day12_test")
+    val testInput = ProgramGraph(readInput("Day12_test"))
     check(part1(testInput) == 6)
 
-    val input = readInput("Day12")
+    val input = ProgramGraph(readInput("Day12"))
     check(part1(input) == 141)
 }
  
