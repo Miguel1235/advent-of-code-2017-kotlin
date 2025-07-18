3a633744b57f90f46124c4194171e902b0508d11
Miguel Del Corso <migueldelcorso@gmail.com>
2025-07-18 11:40:39 -0300

    Started day13, not finished yet

---
+++ src/Day13.kt
@@ -0,0 +1,60 @@
+private fun part1(input: Map<Int, List<String>>): Int {
+    var result = input
+//    var currentLayer = input.keys.first()
+
+//    repeat(10) {
+//        result = moveLasers(result)
+//        currentLayer = movePlayer(input, currentLayer)
+//    }
+
+    for(layer in input.keys) {
+        println("we are in the layer: $layer")
+        result = moveLasers(result)
+        println(result)
+    }
+    return 0
+}
+
+private fun part2(input: List<String>): Int {
+    return 0
+}
+
+private fun movePlayer(input: Map<Int, List<String>>, currentLayer: Int): Int {
+    val keys = input.keys.toList()
+    val nextIndex = (keys.indexOf(currentLayer) + 1) % keys.size
+    return keys[nextIndex]
+}
+
+private fun parseInput(input: List<String>): Map<Int, List<String>> {
+    return buildMap {
+        for(line in input) {
+            val (layer, range) = line.split(": ").map { it.toInt() }
+            put(layer, MutableList(range) { " " }.apply { this[0] = "S" })
+        }
+    }
+}
+
+private fun moveLasers(input: Map<Int, List<String>>): Map<Int, List<String>> {
+    return input.mapValues { (_, range) ->
+        val current = range.indexOf("S")
+        val next = (current + 1) % range.size
+        List(range.size) { i ->
+            when (i) {
+                next -> "S"
+                current -> " "
+                else -> range[i]
+            }
+        }
+    }
+}
+
+fun main() {
+    val testInput = parseInput(readInput("Day13_test"))
+    check(part1(testInput) == 0)
+//    check(part2(testInput) == 0)
+     
+//    val input = readInput("Day13")
+//    check(part1(input) == 0)
+//    check(part2(input) == 0)
+}
+ 
