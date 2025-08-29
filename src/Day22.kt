1aab4651f7c96100ea312f40b89b8ff2475a062d
Miguel Del Corso <migueldelcorso@gmail.com>
2025-08-29 10:14:47 -0300

    Improved code on day22

--- src/Day22.kt
+++ src/Day22.kt
@@ -1,68 +1,39 @@
-private fun part1(gridMap: MutableMap<Pair<Int, Int>, Char>): Int {
+private fun part(inputMap: Map<Pair<Int, Int>, Char>, isPart1: Boolean = true): Int {
+    val gridMap = inputMap.toMutableMap()
     var point = Pair(gridMap.keys.maxOf { it.first } / 2, gridMap.keys.maxOf { it.second } / 2)
     var direction = Directions.UP
     var infections = 0
-
-    repeat(10_000) {
+    repeat(if (isPart1) 10_000 else 10_000_000) {
         val (r, c) = point
         val node = gridMap.getOrElse(Pair(r, c)) { '.' }
-        if (node == '.') {
-            direction = when(direction) {
-                Directions.UP -> Directions.LEFT
-                Directions.LEFT -> Directions.DOWN
-                Directions.RIGHT -> Directions.UP
-                Directions.DOWN -> Directions.RIGHT
-            }
-            gridMap[point] = '#'
-            infections++
-        } else {
-            direction = when(direction) {
-                Directions.UP -> Directions.RIGHT
-                Directions.LEFT -> Directions.UP
-                Directions.RIGHT -> Directions.DOWN
-                Directions.DOWN -> Directions.LEFT
-            }
-            gridMap[point] = '.'
-        }
-        point = Pair(r + direction.rowDelta, c + direction.colDelta)
-    }
-    return infections
-}
-
-private fun part2(gridMap: MutableMap<Pair<Int, Int>, Char>): Int {
-    var point = Pair(gridMap.keys.maxOf { it.first } / 2, gridMap.keys.maxOf { it.second } / 2)
-    var direction = Directions.UP
-    var infections = 0
-
-    repeat(10_000_000) {
-        val (r, c) = point
-        val node = gridMap.getOrElse(Pair(r, c)) { '.' }
-
-        when(node) {
+        when (node) {
             '.' -> {
-                direction = when(direction) {
+                direction = when (direction) {
                     Directions.UP -> Directions.LEFT
                     Directions.LEFT -> Directions.DOWN
                     Directions.RIGHT -> Directions.UP
                     Directions.DOWN -> Directions.RIGHT
                 }
-                gridMap[point] = 'W'
+                if (isPart1) {
+                    gridMap[point] = '#'
+                    infections++
+                } else gridMap[point] = 'W'
             }
             'W' -> {
                 infections++
                 gridMap[point] = '#'
             }
             '#' -> {
-                direction = when(direction) {
+                direction = when (direction) {
                     Directions.UP -> Directions.RIGHT
                     Directions.LEFT -> Directions.UP
                     Directions.RIGHT -> Directions.DOWN
                     Directions.DOWN -> Directions.LEFT
                 }
-                gridMap[point] = 'F'
+                gridMap[point] = if (isPart1) '.' else 'F'
             }
             'F' -> {
-                direction = when(direction) {
+                direction = when (direction) {
                     Directions.UP -> Directions.DOWN
                     Directions.LEFT -> Directions.RIGHT
                     Directions.RIGHT -> Directions.LEFT
@@ -76,24 +47,23 @@ private fun part2(gridMap: MutableMap<Pair<Int, Int>, Char>): Int {
     return infections
 }
 
-private fun parseInput(input: List<String>): MutableMap<Pair<Int, Int>, Char> {
+private fun parseInput(input: List<String>): Map<Pair<Int, Int>, Char> {
     return buildMap {
-        for(r in 0..<input.size) {
-            for(c in 0..<input.size) {
+        for (r in 0..<input.size) {
+            for (c in 0..<input.size) {
                 put(Pair(r, c), input[r][c])
             }
         }
-    }.toMutableMap()
+    }
 }
 
 fun main() {
     val testInput = parseInput(readInput("Day22_test"))
-    check(part1(testInput) == 5587)
-    check(part2(testInput) == 2511944)
+    check(part(testInput) == 5587)
+    check(part(testInput, false) == 2511944)
 
     val input = parseInput(readInput("Day22"))
-    check(part1(input) == 5348)
-    check(part2(input) == 2512225)
-
+    check(part(input) == 5348)
+    check(part(input, false) == 2512225)
 }
  
